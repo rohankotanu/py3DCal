fa5bf8dd7fb97d56ed3ce114c9dd32d530758530
Rohan Kota <rohankota@rohans-mbp-157.ads.northwestern.edu>
2026-02-12 18:01:10 -0600

    updated library version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as f:
 
 setup(
     name='py3dcal',
-    version='2.0.0',
+    version='1.1.0',
     url="https://github.com/rohankotanu/py3DCal",
     author="Rohan Kota",
     author_email="rohankota2026@u.northwestern.edu",
